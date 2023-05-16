0e8ab0a6bafb7f213bce747ccff04fad5a8c013b
Paul Stahr <paul.stahr@gmx.de>
2023-05-16 16:27:29 +0200

    Fixed Video-unittest

--- svidreader/test_filtergraph.py
+++ svidreader/test_filtergraph.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class TestStringMethods(unittest.TestCase):
     def test_named_graph(self):
-    	filtergraph.create_filtergraph_from_string([],'reader=input=../test/cubes.mp4[vid];[vid]cache[out]')
+    	filtergraph.create_filtergraph_from_string([],'reader=input=./test/cubes.mp4[vid];[vid]cache[cached];[cached]tblend[out]')
 
 
 if __name__ == '__main__':
