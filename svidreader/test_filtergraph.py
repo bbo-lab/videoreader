57ecd5a969e20b18283d5d854453333d0a327cff
Paul Stahr <paul.stahr@gmx.de>
2023-06-06 11:48:45 +0200

    Extended testing

--- svidreader/test_filtergraph.py
+++ svidreader/test_filtergraph.py
@@ -4,8 +4,8 @@ import numpy as np
 
 class TestStringMethods(unittest.TestCase):
     def test_named_graph(self):
-    	filtergraph.create_filtergraph_from_string([],'reader=input=./test/cubes.mp4[vid];[vid]cache[cached];[cached]tblend[out]')
-
+        fg = filtergraph.create_filtergraph_from_string([],'reader=input=./test/cubes.mp4[vid];[vid]cache[cached];[cached]tblend[out]')
+        fg['out'].get_data(50)
 
 if __name__ == '__main__':
     unittest.main()
