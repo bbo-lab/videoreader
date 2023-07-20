e7ebbd8e347c921e8cd32d48bccb568d2520641c
Paul Stahr <paul.stahr@gmx.de>
2023-07-20 22:08:09 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     ],
     packages=['svidreader'],
     include_package_data=True,
-    install_requires=["imageio", "bbo_ccvtools", "av", "numpy", "scipy"],
+    install_requires=["imageio", "bbo_ccvtools", "av", "numpy", "scipy", "pyyaml", "bbo-calibcamlib"],
 )
