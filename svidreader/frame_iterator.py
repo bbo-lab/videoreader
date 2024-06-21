32a6b75f45d1ffd7eeb7590e8a5ae1cf5c86b8d1
Paul Stahr <paul.stahr@gmx.de>
2024-06-21 16:26:08 +0200

    Changed interface of frame_iterator

--- svidreader/frame_iterator.py
+++ svidreader/frame_iterator.py
@@ -6,17 +6,20 @@ import numpy as np
 
 
 class FrameIterator(VideoSupplier):
-    def __init__(self, inputs, iterator=None, jobs=1, force_type=None):
-        super().__init__(n_frames=inputs[0].n_frames, inputs=inputs)
-        self.iterator = iterator
+    def __init__(self, input, iterator=None, jobs=1, force_type=None):
+        super().__init__(n_frames=input.n_frames, inputs=(input,))
+        self.jobs = jobs
+        self.force_type = force_type
+        self.iterator = range(self.n_frames) if iterator is None else iterator
 
-        iterator = range(self.n_frames) if iterator is None else iterator
+    def read(self, index, force_type=np, noreturn=False):
+        image = self.inputs[0].read(index=index, force_type=force_type)
+        return None if noreturn else image
 
-        if jobs != 1:
-            thread_map(self.read, iterator, kwargs={'force_type', force_type}, max_workers=jobs, chunksize=1)
+    def run(self):
+        if self.jobs != 1:
+            thread_map(lambda index: self.read(index=index, force_type=self.force_type, noreturn=True), self.iterator, max_workers=self.jobs,
+                       chunksize=1)
         else:
-            for frame_idx in tqdm(iterator):
-                self.read(frame_idx, force_type=force_type)
-
-    def read(self, index, force_type=np):
-        return self.inputs[0].read(force_type=force_type)
+            for frame_idx in tqdm(self.iterator):
+                self.read(frame_idx, force_type=self.force_type)
