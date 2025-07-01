a21f22de570974ea8e6fc7c4a4e33a80edc4c50a
Paul Stahr <paul.stahr@gmx.de>
2025-07-01 10:08:42 +0200

    Improved map-coordinates to also accept another reader as mapping

--- svidreader/filter/map_coordinates.py
+++ svidreader/filter/map_coordinates.py
@@ -4,25 +4,25 @@ import numpy as np
 
 class MapCoordinates(VideoSupplier):
     def __init__(self, reader, image_points, interpolation_order=1):
-        super().__init__(n_frames=reader.n_frames, inputs=(reader,))
+        inputs = (reader,image_points) if isinstance(image_points, VideoSupplier) else (reader,)
+        super().__init__(n_frames=reader.n_frames, inputs=inputs)
         self.interpolation_order = interpolation_order
         self.image_to_int = {}
-        self.image_points = np.moveaxis(image_points, 2, 0)
+        self.image_points = image_points if isinstance(image_points, VideoSupplier) else np.moveaxis(image_points, 2, 0)
 
     def get_image_float_to_int(self, xp):
         res = self.image_to_int.get(xp, None)
-        if res is not None:
-            return res
-        def image_float_to_int(image, xp=xp):
-            image = xp.round(image)
-            image = xp.clip(image, 0, 255)
-            return image.astype(xp.uint8)
-        if xp == np:
-            import numba as nb
-            res = nb.njit(image_float_to_int)
-        else:
-            res = xp.fuse(self.image_to_int)
-        self.image_to_int[xp] = res
+        if res is None:
+            def image_float_to_int(image, xp=xp):
+                image = xp.round(image)
+                image = xp.clip(image, 0, 255)
+                return image.astype(xp.uint8)
+            if xp == np:
+                import numba as nb
+                res = nb.njit(image_float_to_int)
+            else:
+                res = xp.fuse(self.image_to_int)
+            self.image_to_int[xp] = res
         return res
 
     def read(self, index, force_type=np):
@@ -38,9 +38,13 @@ class MapCoordinates(VideoSupplier):
         frame = self.inputs[0].read(index, force_type=force_type)
 
         frame = frame.astype(force_type.float32)
+        image_points = self.image_points
+        if isinstance(image_points, VideoSupplier):
+            image_points = image_points.read(index, force_type=force_type)
+            image_points = np.moveaxis(image_points, 2, 0)
 
         perspective_image = [
-            map_coordinates(frame[:, :, i], self.image_points, mode='constant', cval=0, order=self.interpolation_order) for i in
+            map_coordinates(frame[:, :, i], image_points, mode='constant', cval=0, order=self.interpolation_order) for i in
             range(frame.shape[2])]
         perspective_image = force_type.stack(perspective_image, axis=2)
-        return self.get_image_float_to_int(force_type)(perspective_image)
+        return self.get_image_float_to_int(xp=force_type)(perspective_image)
